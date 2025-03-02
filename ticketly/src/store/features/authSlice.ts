70a97ecd06dff5dd5ed76f584c5d342b85bbea3e
Abubeker Abduljelil <abubekera87@gmail.com>
2025-03-02 08:00:55 +0300

    Setup redix

---
+++ ticketly/src/store/features/authSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+  _id: string;
+  name: string;
+  email: string;
+  token: string;
+}
+
+interface AuthState {
+  user: User | null;
+  isAuthenticated: boolean;
+}
+
+const initialState: AuthState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+      localStorage.setItem("user", JSON.stringify(action.payload));
+    },
+    logoutUser: (state) => {
+      console.log("hello");
+      state.isAuthenticated = false;
+      state.user = null;
+      localStorage.removeItem("user");
+    },
+    loadUser: (state) => {
+      const user = localStorage.getItem("user");
+      if (user) {
+        state.user = JSON.parse(user);
+        state.isAuthenticated = true;
+      }
+    },
+  },
+});
+
+export const { loginUser, logoutUser, loadUser } = authSlice.actions;
+export default authSlice.reducer;
