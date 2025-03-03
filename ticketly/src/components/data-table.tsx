9e0c93cfea5158292ad6c686230db65afca3be94
Abubeker Abduljelil <abubekera87@gmail.com>
2025-03-03 04:06:04 +0300

    Fix: table z index

--- ticketly/src/components/data-table.tsx
+++ ticketly/src/components/data-table.tsx
@@ -40,7 +40,7 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div className="rounded-md border">
+    <div className="rounded-md border  -z-10">
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
