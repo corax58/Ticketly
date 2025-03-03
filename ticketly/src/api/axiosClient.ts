60a61edf27319c8d17f784b0a3955b92c5c50462
Abubeker Abduljelil <abubekera87@gmail.com>
2025-03-03 04:01:11 +0300

    create axios api instance

--- ticketly/src/api/axiosClient.ts
+++ ticketly/src/api/axiosClient.ts
@@ -1,24 +1,20 @@
 import axios from "axios";
 
-const token = localStorage.getItem("token"); // Get token from storage
-
-// Create an Axios instance
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Add an interceptor to attach the token
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("user");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("user");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
